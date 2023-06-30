f8c5419dcaefd03d3474b18475eb5b2519336977
jorgelnc <jorgenavacolina@gmail.com>
2023-06-30 16:10:55 -0500

    task 1 added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+let newGallery = galleryItems.map((item) => {
+    const li = `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+        <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+        />
+    </a>
+</li>`;
+    return li;
+  })
+  .join("");
+
+gallery.innerHTML = newGallery;
+
+gallery.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+    
+    let instance = basicLightbox.create(`<img src="${e.target.src}" alt="${e.target.alt}"/>`);
+    instance.show();
+
+    document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+    instance.close();
+    }
+    });
+});
