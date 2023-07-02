d915043afb9104a046582e75a3cf39fe2ef972a9
jorgelnc <jorgenavacolina@gmail.com>
2023-07-01 23:14:55 -0500

    task 2 added

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+let newGallery = galleryItems.map((item) => {
+    const li = `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`;
+    return li;
+  })
+  .join("");
+
+gallery.innerHTML = newGallery;
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250ms",
+});
+
+
